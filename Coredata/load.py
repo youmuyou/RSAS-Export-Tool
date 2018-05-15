ef6ffc4374e72cae4b43874234928ec9b035f054
webingio <36809253+webingio@users.noreply.github.com>
2018-05-15 20:24:48 +0800

    Update load.py

--- Coredata/load.py
+++ Coredata/load.py
@@ -11,34 +11,23 @@ class File_re(object):
         self.file_re = '.*?.zip'
         self.uzip_re = '.*?.html'
         self.title_re = '<th width="120">任务名称</th>.*?<td>(.*?)</td>'
-        #正则，用于获取2.1 漏洞概况所有内容
         self.vul_list_re = '(<table id="vuln_list" class="report_table">.*?</table>)'
-        #正则，用于获取2.2 漏洞详情所有内容
         self.vul_detail_re = '(<div id="vul_detail">.*?</div>)'
-        #正则，用于获取2.2 漏洞详情所有内容,并分开内容
         self.vul_details_re = '(<tr class="solution.*?">.*?<td>.*?<table class="report_table plumb".*?>.*?</table>.*?</td>.*?</tr>)'
-        #正则，用于获取IP地址、主机名、操作系统
         self.host_re = '(<td valign="top" style="width:50%;">.*?<table class="report_table plumb">.*?<tbody>.*?<th width="120">IP地址</th>.*?</tbody>.*?</table></td>)'
 
 
 def load_date():
     clean_date()
-    #获取实参目录
     folder_name = str(sys.argv[1])
-    #获取目录下的所有文件
     dirList = os.listdir(folder_name)
     for name in dirList:
-        #查找zip压缩包(.*?.zip)
         all_file_name = re.findall(File_re().file_re,name)
         for file_name in all_file_name:
-            #打开压缩包文件
             uzip = zipfile.ZipFile(folder_name+'/'+file_name)
-            #迭代所有文件名
             for uzip_content in uzip.namelist():
-                #只获取正则内的文件名（.*?.html）
                 all_uzip_content = re.findall(File_re().uzip_re,uzip_content)
                 for all_uzip in all_uzip_content:
-                    #打开这些文件，按照以下正则分别获取内容，写入文件（辅助作用）
                     htmlcont = uzip.open(all_uzip).read().decode('utf8')
 
                     title = re.findall(File_re().title_re,htmlcont,re.S|re.M)
@@ -87,6 +76,5 @@ def load_date():
 
 def clean_date():
     with open('./Coredata/database.mdb', 'w') as content:
-        #清空文件里的数据
         content.write('')
         print('\n删除缓存成功！')
