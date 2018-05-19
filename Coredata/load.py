5fa790d009b7411d09eb8fb40cd62dc3c168b3b0
webingio <36809253+webingio@users.noreply.github.com>
2018-05-19 22:15:11 +0800

    Update load.py

--- Coredata/load.py
+++ Coredata/load.py
@@ -1,8 +1,9 @@
 import os
-import sys
 import re
-import zipfile
+import sys
 import html
+import shutil
+import zipfile
 import time
 
 class File_re(object):
@@ -18,7 +19,6 @@ class File_re(object):
 
 
 def load_date():
-    clean_date()
     folder_name = str(sys.argv[1])
     dirList = os.listdir(folder_name)
     for name in dirList:
@@ -32,49 +32,59 @@ def load_date():
 
                     title = re.findall(File_re().title_re,htmlcont,re.S|re.M)
                     for title_content in title:
-                        with open('./Coredata/database.mdb', 'a') as content:
-                            content.write('<python>python<python>\n')
+                        with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                             content.write('<python>title<python>')
                             content.write(html.unescape(title_content))
                             content.write('<python>title</python>\n')
 
+                        with open('./Coredata/database.mdb','a') as content:
+                            content.write('./Coredata/temp/'+html.unescape(title_content)+'.mdb\n')
+
+
                     host = re.findall(File_re().host_re,htmlcont,re.S|re.M)
                     for host_content in host:
-                        with open('./Coredata/database.mdb', 'a') as content:
+                        with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                             content.write('<python>host<python>\n')
                             content.write(html.unescape(host_content))
                             content.write('\n<python>host</python>\n')
 
                     vul_list = re.findall(File_re().vul_list_re,htmlcont,re.S|re.M)
                     for list_content in vul_list:
-                        with open('./Coredata/database.mdb', 'a') as content:
+                        with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                             content.write('<python>vul_list<python>\n')
                             content.write(html.unescape(list_content))
                             content.write('\n<python>vul_list</python>\n')
 
                     vul_detail = re.findall(File_re().vul_detail_re,htmlcont,re.S|re.M)
                     for detail_content in vul_detail:
-                        with open('./Coredata/database.mdb', 'a') as content:
+                        with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                             content.write('<python>vul_detail<python>\n')
 
                         vul_details = re.findall(File_re().vul_details_re,detail_content,re.S|re.M)
                         for list_details in vul_details:
-                            with open('./Coredata/database.mdb', 'a') as content:
+                            with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                                 content.write('<python>vul_details<python>\n')
                                 content.write(html.unescape(list_details))
                                 content.write('\n<python>vul_details</python>\n')
 
 
-                        with open('./Coredata/database.mdb', 'a') as content:
+                        with open('./Coredata/temp/%s.mdb'%title_content, 'a') as content:
                             content.write('\n<python>vul_detail</python>\n')
-
-            with open('./Coredata/database.mdb', 'a') as content:
-                content.write('<python>python</python>\n')
 
             print('提取 %s'%file_name+' 完成！')
 
 
-def clean_date():
+def start_date():
+    try:
+        shutil.rmtree('./Coredata/temp')
+        time.sleep(0.1)
+        os.mkdir('./Coredata/temp')
+    except Exception as e:
+        os.mkdir('./Coredata/temp')
     with open('./Coredata/database.mdb', 'w') as content:
         content.write('')
-        print('\n删除缓存成功！')
+
+
+def end_date():
+    shutil.rmtree('./Coredata/temp')
+    os.remove('./Coredata/database.mdb')
